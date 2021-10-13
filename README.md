f5377f8a03eded65a5921959375e1ee77cee42fd
Harsh Deep <hrshdeep51@gmail.com>
2021-10-13 16:00:55 -0500

    updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# fgit: Fancy Git Client 
+
+Command lne git client with fancy interface features!
+
+Based off a [tutorial workshop](https://125summer.tech/cli) I did in Summer 2021 as part of UIUC Summer of Side Projects.
+
---
+++ fgit.py
@@ -0,0 +1,49 @@
+#!python3
+
+from plumbum import cli 
+from pyfiglet import Figlet
+from plumbum.cmd import ls, git
+from questionary import checkbox
+from rich.progress import Progress
+
+import time
+
+def print_banner(text: str):
+    print(Figlet(font='slant').renderText(text))
+
+def get_files():
+    ls_output = ls().strip()
+    files = ls_output.split("\n")
+    return files
+
+def display_push_progress():
+    # Note: Actually getting progress from git is surprisingly non-trivial
+    # So I've hard coded an example for now, but in real code, emit things as they happen
+    with Progress() as progress:
+        push_progress = progress.add_task("Pushing files :partying_face:")
+
+        for i in range(100):
+            progress.update(push_progress, advance=1)
+            time.sleep(0.1)
+
+class FancyGitAdd(cli.Application):
+    VERSION = "1.3"
+    commit = cli.Flag(['c', 'commit'], help="Commits the added files")
+    push = cli.Flag(['p', 'push'], help="Pushes the added files")
+    def main(self):
+        print_banner("Git Fancy add")
+
+        files = get_files()
+
+        selected_files = checkbox("Select a file", choices=files).ask()
+        git('add', selected_files)
+
+        if self.commit:
+            git('commit', '-m', 'updates')
+
+        if self.push:
+            git('push')
+            display_push_progress()
+
+if __name__ == "__main__":
+    FancyGitAdd()
